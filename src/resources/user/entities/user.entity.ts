413c5c29b84fa7aa8bd39e6fd45dec783710e6e7
Muhammad Taaqbeel <53563490+Taqbeel@users.noreply.github.com>
2024-01-29 00:03:20 +0500

    user entity

--- src/resources/user/entities/user.entity.ts
+++ src/resources/user/entities/user.entity.ts
@@ -15,19 +15,11 @@ export class User extends EntityBase {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ nullable: true })
   firstName: string;
 
   @Column({ nullable: true })
   lastName: string;
-
-  @Column({
-    unique: true,
-  })
-  phoneNumber: string;
-
-  @Column({ unique: true, nullable: true })
-  email: string;
 
   @Column()
   barCode: string;
@@ -35,31 +27,11 @@ export class User extends EntityBase {
   @Column()
   employeeCode: string;
 
-  @Column()
+  @Column({ nullable: true })
   noOfLeaves: number;
 
-  @Column()
+  @Column({ nullable: true })
   role: string;
-
-  @Column({ nullable: true })
-  passportNumber: string;
-
-  @Column({ nullable: true })
-  emirateId: string;
-
-  @Column({ nullable: true })
-  @IsDateFormat()
-  passportExpiry: Date;
-
-  @Column({ nullable: true })
-  @IsDateFormat()
-  visaExpiry: Date;
-
-  @Column()
-  company: string;
-
-  @Column({ nullable: true })
-  companyTitle: string;
 
   @Column({ default: false, nullable: true })
   checkedIn: boolean;
@@ -72,6 +44,90 @@ export class User extends EntityBase {
 
   @OneToMany(() => Attendance, (attendance) => attendance.user)
   attendance: Attendance[];
+
+  @Column({ nullable: true })
+  emirateId: string;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  dateJoining: Date;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  dateBirth: Date;
+
+  @Column({ nullable: true })
+  passportNumber: string;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  passportIssue: Date;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  passportExpiry: Date;
+
+  @Column({ nullable: true })
+  newPassportNumber: string;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  newPassportIssue: Date;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  newPassportExpiry: Date;
+
+  @Column({ nullable: true })
+  visaNumber: string;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  visaIssue: Date;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  visaExpiry: Date;
+
+  @Column({ nullable: true })
+  visaStatus: string;
+
+  @Column({ nullable: true })
+  visaValidity: string;
+
+  @Column({ nullable: true })
+  nationality: string;
+
+  @Column({ nullable: true })
+  workPermit: string;
+
+  @Column({ nullable: true })
+  workPermitPersonal: string;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  workPermitIssue: Date;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  workPermitExpiry: Date;
+
+  @Column({ nullable: true })
+  newWorkPermit: string;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  newWorkPermitIssue: Date;
+
+  @Column({ nullable: true })
+  @IsDateFormat()
+  newWorkPermitExpiry: Date;
+
+  @Column()
+  company: string;
+
+  @Column({ nullable: true })
+  companyTitle: string;
 
   // @OneToOne(() => Media, (media) => media.user)
   // media: Media;
